b5f274f85efd9fb4b9d537259d635aa1ea8a4115
dumanensar <dumanensar@hotmail.com>
2015-09-05 12:04:23 +0300

    Update

--- map.js
+++ map.js
@@ -1,28 +1,32 @@
 var map = {
-    north: 42.296048,
-    south: 34.584382,
-    east: 44.860279,
-    west: 25.585485,
+    north: 42.673093,
+    south: 34.632070,
+    east: 45.287194,
+    west: 25.555186,
 
-    //north: 41.896048,
-    //south: 27.584382,
+    iconHeight: 33,
+    iconWidth: 33,
+
+    popupHeight: 100,
+    popupWidth: 150,
 
     data: [
-        //{id:'', x:0, y:0} 39.931502, 32.856155
-        { id: 'izmir', x: 27.145878, y: 38.429399 },
-        { id: 'istanbul', x: 28.989518, y: 41.073903 },
-        { id: 'kars', x: 43.090693, y: 40.595234 },
-        { id: 'ankara', x: 32.856155, y: 39.931502 },
+        //{ name: 'izmir', x: 27.145878, y: 38.429399, html: "<a href='#'>Html Code</a>" },
     ],
+
+    setData: function(data){
+        this.data = data;
+        this.run();
+    },
 
     getSize: function () {
         var x = $("#map").width();
         var y = $("#map").height();
         console.log("size: " + x + " - " + y);
-        return {x: x, y: y};
+        return { x: x, y: y };
     },
 
-    setSize: function(x, y){
+    setSize: function (x, y) {
         this.setX(x);
         this.setY(y);
     },
@@ -41,11 +45,21 @@ var map = {
         return k;
     },
 
-    run: function () {
+    resize: function () {
+        var x = window.innerWidth;
+        map.setX(x);
+    },
+
+    runIcons: function () {
         this.clear();
         for (var i = 0; i < this.data.length; i++) {
-            addIconGeoraphic(this.data[i].id, this.data[i].x, this.data[i].y);
+            addIconGeoraphic(i, this.data[i].name, this.data[i].x, this.data[i].y, this.data[i].html);
         }
+    },
+
+    run: function () {
+        this.resize();
+        this.runIcons();
     },
 
     clear: function () {
@@ -53,34 +67,61 @@ var map = {
     }
 }
 
-
-function resizeMap() {
-    var x = window.innerWidth;
-    map.setX(x);
-}
-
-function addIconGeoraphic(id, x, y) {
+function addIconGeoraphic(id, name, x, y, html) {
     var k = map.getK();
 
-    x = Math.abs(x - map.west) * k;
-    y = (Math.abs(map.north - map.south) - (y - map.south)) * k;
+    x = Math.abs(x - map.west) * k*0.99;
+    y = Math.abs(map.north - y) * k * 1.18;
 
     console.log("geo: " + x + " - " + y);
-    addIcon(id, x, y);
+    addIcon(id, name, x, y, html);
 }
 
-function addIcon(id, x, y) {
-    console.log(id + ': ' + x + ' - '  + y)
-    $("#icons").html($("#icons").html() + "<img id='icon_" + id + "' src='icon.png' class='icon' style='top:" + y + "px;left:" + x + "px;'>");
+function addIcon(id, name, x, y, html) {
+    var iconX = map.iconWidth;
+    var iconY = map.iconHeight;
+
+    x = x - (iconX / 2);
+    y = y - iconY;
+
+    var px = x + iconX;
+    var py = y - iconY;
+
+    if (py < 0) {
+        py = 0;
+    }
+
+    if (px + map.popupWidth > map.getSize().x) {
+        px = x - map.popupWidth;
+    }
+
+    console.log(id + ': ' + x + ' - ' + y)
+    $("#icons").html($("#icons").html() + "<img name='icon_"+name+"' id='icon_" + id + "' src='icon.png' class='icon' style='top:" + y + "px;left:" + x + "px; height:" + map.iconHeight + "px; width:" + map.iconWidth + "px;' onclick='openPopup(" + id + ")'>");
+    $("#icons").html($("#icons").html() + "<div name='popup_" + name + "' id='popup_" + id + "' src='icon.png' class='popup' style='top:" + py + "px;left:" + px + "px; height:" + map.popupHeight + "px; width:" + map.popupWidth + "px;'>" + html + "</div>");
 }
 
-//$(document).ready(function () {
-//    resizeMap();
-//    console.log(map.getK());
-//    map.run();
+function openPopup(id) {
+    closePopups();
+    $("#popup_" + id).show();
+}
 
-//    $(window).resize(function () {
-//        resizeMap();
-//        map.run();
-//    });
-//});
+function closePopup(id) {
+    $("#popup_" + id).hide();
+}
+
+function closePopups() {
+    $(".popup").hide();
+}
+
+$(document).ready(function () {
+    $("#map").click(function () {
+        closePopups();
+    });
+
+    map.run();
+
+    $(window).resize(function () {
+        map.run();
+    });
+});
+
